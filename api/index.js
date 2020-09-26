3c60ca03b5514e10decda4049f003bb10567e1ab
Moises Veliz <msveliz16@gmail.com>
2020-09-26 02:42:43 -0400

    test

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParse = require('body-parse')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express
 
-app.use(bodyParse.json())
+app.use(bodyParser.json())
 app.use(cors())
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
